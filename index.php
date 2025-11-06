510dc2253ecdb616ca9434f8df51abe7cd2140fd
Shadrack Berdah <shadrack.berdah@ashesi.edu.gh>
2025-11-06 23:23:56 +0000

    Updated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once 'settings/core.php';
-require_once 'controllers/product_controller.php';
+require_once __DIR__ . '/settings/core.php';
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
